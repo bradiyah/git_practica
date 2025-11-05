6eea0db91b11c15843ecdbbe65a6f58804fb4dfe
a25elenapg <htmtf111@gmail.com>
2025-11-05 16:13:55 +0100

    funcionalidad de registro hecha

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("version inicial del proyecto");
+    System.out.println("funcionalidad de registro implementada");
   }
 }
